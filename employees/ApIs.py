3a11c8878d2f3658f0204095c579e632b605257f
mousa8080 <ahmedmousa010203@gmail.com>
2025-02-10 08:25:34 +0200

    asd

--- employees/ApIs.py
+++ employees/ApIs.py
@@ -14,31 +14,7 @@ class EmployeesDetailsAPI(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employees.objects.all()
     serializer_class = EmployeesSerializer
     
-class ExampleApi(APIView):
-
-    """
-    GET-->    api/emplyees/ list of employees
-    POST --> api/emplyees/ {emplyee} 200 , {}
-    GET --> api/emplyees/<int:id> one emplyee
-    PUT , patch -->api/emplyees/<int:id>
-    DELETE --> api/emplyees/<int:id>
-
-    
-    
-    """
-    def get(self, request):
-        return Response({'hello': 'world'})
-    def post(self, request):
-        return Response({'hello': 'world'})
-    def put(self, request):
-        return Response({'hello': 'world'})
-    def patch(self, request):
-        return Response({'hello': 'world'})
-    def delete(self, request):
-        return Response({'hello': 'world'})
-
 class postionApiView(generics.ListCreateAPIView):
     queryset = postion.objects.all()
-    queryset = postion.objects.all().delete()
     serializer_class = postionSerializer
 
