25a2d4e9730c800ccc9b1adf526b2127e7875eae
mousa8080 <ahmedmousa010203@gmail.com>
2025-02-10 08:34:04 +0200

    يا مسهل

--- hr_system/settings.py
+++ hr_system/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'hr_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
+        'NAME': 'mousa',
+        'USER': 'postgres',
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
+        'HOST': 'localhost',
+        'PORT': '5432',
         
     }
 }
@@ -139,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
